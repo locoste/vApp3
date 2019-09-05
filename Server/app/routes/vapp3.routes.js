9db8de83e0f408c276ddd3b5d9d479d493e0bce2
lcoste <l.coste@univ-lyon2.fr>
2019-09-05 15:04:35 +0200

    put env variable for database connection

--- Server/app/routes/vapp3.routes.js
+++ Server/app/routes/vapp3.routes.js
@@ -15,11 +15,11 @@ module.exports = function(app) {
     var fs = require('fs');
 
     var options = {
-      host: 'localhost',
-      port: 3306,
-      user: 'root',
-      password: 'Lamoule07130',
-      database: 'vapp3'
+      host: process.env.DATABASE_HOST, 
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE
     };
 
     var sessionStore = new MySQLStore(options);
